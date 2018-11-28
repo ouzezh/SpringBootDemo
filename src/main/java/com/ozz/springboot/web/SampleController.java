f1ffd5fbcfd7a4e80d5a6c78ab73d78cf1878c1c
ouzezhou <ouzezhou@xdf.cn>
2018-11-28 16:47:46 +0800

    test

--- src/main/java/com/ozz/springboot/web/SampleController.java
+++ src/main/java/com/ozz/springboot/web/SampleController.java
@@ -5,9 +5,9 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ozz.springboot.dao.SampleDao;
@@ -25,7 +25,7 @@ public class SampleController {
     return dao.sevice(p);
   }
 
-  @RequestMapping(value = "/test/{id}", method = RequestMethod.GET)
+  @GetMapping(value = "/test/{id}")
   public String testPathParam(@PathVariable Long id) {
     return "id=" + id;
   }

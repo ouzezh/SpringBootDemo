a5667ea48d8864a925413f9edeb3f87f560fcd27
ouzezhou <ouzezhou@xdf.cn>
2019-01-09 17:24:02 +0800

    remove jsonp

--- src/main/java/com/ozz/springboot/component/advice/JsonpResponseBodyAdvice.java
+++ src/main/java/com/ozz/springboot/component/advice/JsonpResponseBodyAdvice.java
@@ -1,16 +1,19 @@
 package com.ozz.springboot.component.advice;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
-
+//import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 /**
  * 支持jsonp
  * 
  * 需要返回jsonp格式时在参数中加callback=?即可
+ * 
+ * @deprecated Will be removed as of Spring Framework 5.1, use
+ * <a href="https://docs.spring.io/spring/docs/5.0.x/spring-framework-reference/web.html#mvc-cors">CORS</a> instead.
  */
-@ControllerAdvice(basePackages = "com.ozz.springboot.web")
-public class JsonpResponseBodyAdvice extends AbstractJsonpResponseBodyAdvice {
-  public JsonpResponseBodyAdvice() {
-    super("callback", "jsonp");
-  }
+//@ControllerAdvice(basePackages = "com.ozz.springboot.web")
+//public class JsonpResponseBodyAdvice extends AbstractJsonpResponseBodyAdvice {
+//  public JsonpResponseBodyAdvice() {
+//    super("callback", "jsonp");
+//  }
+//}
+public class JsonpResponseBodyAdvice {
 }

cd02569c739e1032bdf95109cd80ff82b0daaab9
ouzezhou <ouzezhou@JT103068.staff.neworiental.org>
2019-05-16 14:47:19 +0800

    download/upload

--- src/main/java/com/ozz/springboot/web/SampleController.java
+++ src/main/java/com/ozz/springboot/web/SampleController.java
@@ -1,7 +1,13 @@
 package com.ozz.springboot.web;
 
+import java.io.PrintWriter;
+import java.net.URLEncoder;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ozz.springboot.dao.SampleDao;
 
@@ -28,5 +35,34 @@ public class SampleController {
   @GetMapping(value = "/test/{id}")
   public String testPathParam(@PathVariable Long id) {
     return "id=" + id;
+  }
+
+  @RequestMapping(value = "/test/upload")
+  public void testUpload(MultipartFile file) {
+    System.out.println(file.getName());
+    System.out.println(file.getSize());
+  }
+
+  @RequestMapping(value = "/test/download")
+  public void testDownload(HttpServletResponse response) {
+    try {
+      response.reset();
+      response.setContentType("application/octet-stream; charset=utf-8");
+      response.setHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode("test.csv", "UTF-8"));
+
+      PrintWriter out = response.getWriter();
+      String bomStr = new String(new byte[] {(byte) 0xef, (byte) 0xbb, (byte) 0xbf}, "UTF-8");
+      out.print(bomStr);
+
+      CSVFormat format = CSVFormat.EXCEL.withHeader(new String[] {"name"});
+      try(CSVPrinter printer = new CSVPrinter(out, format);) {
+        printer.printRecord("john");
+        printer.print("bob");
+      }
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }

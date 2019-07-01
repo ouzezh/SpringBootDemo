08f993d93963ad92b5818694ee2ad9232583aa5c
ouzezhou <ouzezhou@JT103068.staff.neworiental.org>
2019-07-01 11:18:47 +0800

    table

--- src/main/java/com/ozz/springboot/web/SampleMvcController.java
+++ src/main/java/com/ozz/springboot/web/SampleMvcController.java
@@ -1,5 +1,9 @@
 package com.ozz.springboot.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +11,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SampleMvcController {
+  @SuppressWarnings("serial")
   @GetMapping("/greeting")
-  public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-      model.addAttribute("name", name);
-      return "greeting";
+  public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("books", new ArrayList<Map<String, String>>() {
+      {
+        add(new HashMap<String, String>() {
+          {
+            put("title", "b1");
+            put("author", "a1");
+          }
+        });
+      }
+    });
+    return "greeting";
   }
 }

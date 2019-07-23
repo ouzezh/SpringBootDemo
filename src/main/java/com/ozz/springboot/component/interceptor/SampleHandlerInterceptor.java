6521b6a2ae0298031eed3916719127a2a368dd75
ouzezhou <ouzezhou@JT103068.staff.neworiental.org>
2019-07-23 22:17:46 +0800

    interceptor

--- src/main/java/com/ozz/springboot/component/interceptor/SampleHandlerInterceptor.java
+++ src/main/java/com/ozz/springboot/component/interceptor/SampleHandlerInterceptor.java
@@ -1,5 +1,6 @@
 package com.ozz.springboot.component.interceptor;
 
+import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,7 +12,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public interface SampleHandlerInterceptor extends HandlerInterceptor {
   @Override
   default boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    System.out.println("spring拦截器示例");
+    System.out.println("spring拦截器：preHandle");
     return true;
+  }
+  @Override
+  default void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    System.out.println("spring拦截器：afterCompletion");
   }
 }

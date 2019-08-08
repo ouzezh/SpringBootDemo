94ced8b0461c781705611956a27783309e3786b8
ouzezhou <ouzezhou@JT103068.staff.neworiental.org>
2019-08-08 14:05:26 +0800

    fix config

--- src/main/java/com/ozz/springboot/component/schedule/SampleSchedule.java
+++ src/main/java/com/ozz/springboot/component/schedule/SampleSchedule.java
@@ -3,6 +3,8 @@ package com.ozz.springboot.component.schedule;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +13,15 @@ public class SampleSchedule {
   private Logger log = LoggerFactory.getLogger(getClass());
   private static volatile int scheduleCount = 0;
 
+  @Autowired
+  private Environment env;
+
   /**
    * 固定时间间隔执行，单位：毫秒
    */
 //  @Scheduled(cron="0 0 1 * * ?")
   @Scheduled(fixedDelay = 60000)
   public void schedule() {
-    log.info(String.format("schedule run %d at %s", ++scheduleCount, DateFormatUtils.format(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss")));
+    log.info(String.format("schedule run %d at %s, readConfig: %s", ++scheduleCount, DateFormatUtils.format(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss"), env.getProperty("ozz.sampleConfig")));
   }
 }

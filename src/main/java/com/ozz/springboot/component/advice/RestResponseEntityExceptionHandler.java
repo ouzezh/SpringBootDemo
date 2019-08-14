db1ba9ec0b835a6e6f8f2bbc7471e82ef8f44a5a
ouzezhou <ouzezhou@JT103068.staff.neworiental.org>
2019-08-14 17:29:06 +0800

    error handler

--- src/main/java/com/ozz/springboot/component/advice/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/ozz/springboot/component/advice/RestResponseEntityExceptionHandler.java
@@ -3,12 +3,12 @@ package com.ozz.springboot.component.advice;
 import java.util.Collections;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -16,19 +16,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.ozz.springboot.util.JsonUtil;
 
-/**
- * 针对SQL异常定制化处理，防止数据库信息泄露到前端
- */
 @ControllerAdvice(basePackages = "com.ozz.springboot.web")
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
   private final Logger log = LoggerFactory.getLogger(getClass());
 
   @ExceptionHandler({Exception.class})
   protected ResponseEntity<Object> handleConflict(Exception ex, WebRequest request) {
-    log.error(null, ex);
-    Map<String, Object> map = Collections.singletonMap("message", ex.getMessage() != null ? ex.getMessage() : ex.getClass().getName());
-    MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-    headers.add("Content-Type", "application/json");
-    return new ResponseEntity<>(JsonUtil.toJson(map), headers, HttpStatus.INTERNAL_SERVER_ERROR);
+    return handleExceptionInternal(ex, null, null, HttpStatus.INTERNAL_SERVER_ERROR, request);
+  }
+
+  /**
+   * 异常处理（必须覆盖，否则标准异常处理的提示会没有）
+   *
+   */
+  @Override
+  protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    log.error(body == null ? null : body.toString(), ex);
+
+    if (body == null && ex != null) {
+      Map<String, Object> map = Collections.singletonMap("message", StringUtils.isNotEmpty(ex.getMessage()) ? ex.getMessage() : ex.getClass().getName());
+      body = JsonUtil.toJson(map);
+      if (headers == null) {
+        headers = new HttpHeaders();
+      }
+      headers.add("Content-Type", "application/json");
+    }
+
+    return super.handleExceptionInternal(ex, body, headers, status, request);
   }
 }

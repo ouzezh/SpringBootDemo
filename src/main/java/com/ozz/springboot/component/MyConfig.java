5c40eddf526163155f96b18c4adb2ea095f1c923
ouzezh <ouzezh@163.com>
2020-09-23 13:02:39 +0800

    build

--- src/main/java/com/ozz/springboot/component/MyConfig.java
+++ src/main/java/com/ozz/springboot/component/MyConfig.java
@@ -85,7 +85,7 @@ public class MyConfig implements WebMvcConfigurer {
     FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
     FastJsonConfig config = new FastJsonConfig();
     config.setSerializerFeatures(
-//        SerializerFeature.WriteMapNullValue,        // 是否输出值为null的字段,默认为false,我们将它打开
+//        SerializerFeature.WriteMapNullValue,        // 是否输出值为null的字段,默认为false
 //        SerializerFeature.WriteNullListAsEmpty,     // 将Collection类型字段的字段空值输出为[]
 //        SerializerFeature.WriteNullStringAsEmpty,   // 将字符串类型字段的空值输出为空字符串
         SerializerFeature.DisableCircularReferenceDetect    // 禁用循环引用

9fd76b7726ed06e51ec8a241a209bf3b2d9ab9c9
ouzezh <ouzezh@163.com>
2020-09-27 11:13:32 +0800

    rename

--- src/main/java/com/ozz/springboot/config/aspect/AnalyzeAspect.java
+++ src/main/java/com/ozz/springboot/config/aspect/AnalyzeAspect.java
@@ -1,12 +1,9 @@
 package com.ozz.springboot.config.aspect;
 
 import java.lang.reflect.Method;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.tuple.MutablePair;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -14,35 +11,24 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * 请求执行时间统计
- */
 @Component
 @Aspect
 public class AnalyzeAspect {
-
-  private Logger log = LoggerFactory.getLogger(getClass());
+  // <methodPath, Pair<order, Pair<executeCount, executeTime>>>
+  ThreadLocal<Map<String, MutablePair<Integer, MutablePair<Integer, Long>>>> localSumInfo = new ThreadLocal<>();//<key,<timestamp,<count,time>>>
+  ThreadLocal<Integer> localInvokeOrder = new ThreadLocal<>();
 
   @Pointcut("execution(public * com.ozz.springboot..*.*(..))")
   public void pointcut() {
     // do nothing
   }
 
-  // <methodPath, <order, <executeCount, executeTime>>>
-  ThreadLocal<Map<String, MutablePair<Integer, MutablePair<Integer, Long>>>> sumTime = new ThreadLocal<>();//<key,<timestamp,<count,time>>>
-  ThreadLocal<AtomicInteger> invokeOrder = new ThreadLocal<>();
-
-  @Around("pointcut()")
-  public Object aroundPointcut(ProceedingJoinPoint pjp) throws Throwable {
-    MethodSignature signature = (MethodSignature) pjp.getSignature();
-    Method method = signature.getMethod();
+  private String getInvokerPath(ProceedingJoinPoint pjp, Method method) {
     String methodPath = Optional.ofNullable(method.getAnnotation(RequestMapping.class)).map(item -> item.value()[0]).orElse(null);
     if (methodPath == null) {
       methodPath = Optional.ofNullable(method.getAnnotation(GetMapping.class)).map(item -> item.value()[0]).orElse(null);
@@ -50,9 +36,38 @@ public class AnalyzeAspect {
     if (methodPath == null) {
       methodPath = Optional.ofNullable(method.getAnnotation(PostMapping.class)).map(item -> item.value()[0]).orElse(null);
     }
-    if (methodPath != null) {
-      sumTime.set(new HashMap<>());
-      invokeOrder.set(new AtomicInteger(0));
+    return methodPath;
+  }
+
+  @Around("pointcut()")
+  public Object aroundPointcut(ProceedingJoinPoint pjp) throws Throwable {
+    MethodSignature signature = (MethodSignature) pjp.getSignature();
+    Method method = signature.getMethod();
+
+    Map<String, MutablePair<Integer, MutablePair<Integer, Long>>> sumInfo = localSumInfo.get();
+    String invokerPath = null;
+    if(sumInfo == null) {
+      invokerPath = getInvokerPath(pjp, method);
+      if (invokerPath != null) {
+        localSumInfo.set(new LinkedHashMap<>());
+        localInvokeOrder.set(1);
+      }
+    }
+
+    String key = null;
+    MutablePair<Integer, MutablePair<Integer, Long>> v = null;
+    if (sumInfo != null) {
+      // 调用路径
+      key = String.format("%s.%s", pjp.getTarget().getClass().getName(), method.getName());
+      v = sumInfo.get(key);
+      if (v == null) {
+        // 初始化统计信息
+        v = new MutablePair<>(0, new MutablePair<>(0, new Long(0)));
+        sumInfo.put(key, v);
+        // 排序
+        v.setLeft(localInvokeOrder.get());
+        localInvokeOrder.set(localInvokeOrder.get() + 1);
+      }
     }
 
     // 执行方法
@@ -60,17 +75,7 @@ public class AnalyzeAspect {
     Object object = pjp.proceed();
     ts = System.currentTimeMillis() - ts;
 
-    Map<String, MutablePair<Integer, MutablePair<Integer, Long>>> map = sumTime.get();
-    if (map != null) {
-      // 方法路径
-      String key = String.format("%s.%s", signature.getDeclaringType().getName(), method.getName());
-      MutablePair<Integer, MutablePair<Integer, Long>> v = map.get(key);
-      if (v == null) {
-        v = new MutablePair<>(0, new MutablePair<>(0, new Long(0)));
-        map.put(key, v);
-      }
-      // 排序
-      v.setLeft(invokeOrder.get().incrementAndGet());
+    if(sumInfo != null) {
       // 执行次数
       v.getRight().setLeft(v.getRight().getLeft() + 1);
       // 执行时间
@@ -78,19 +83,13 @@ public class AnalyzeAspect {
     }
 
     // toString
-    if (methodPath != null) {
-      sumTime.remove();
-      invokeOrder.remove();
-      String res = map.entrySet().stream()
-          .sorted(new Comparator<Entry<String, MutablePair<Integer, MutablePair<Integer, Long>>>>() {
-            @Override
-            public int compare(Entry<String, MutablePair<Integer, MutablePair<Integer, Long>>> o1,
-                Entry<String, MutablePair<Integer, MutablePair<Integer, Long>>> o2) {
-              return o1.getValue().getLeft().compareTo(o2.getValue().getLeft());
-            }
-          }).map(item -> String.format("%s: %s, %s", item.getKey(), item.getValue().getRight().getLeft(), item.getValue().getRight().getRight()))
+    if (invokerPath != null) {
+      localSumInfo.remove();
+      localInvokeOrder.remove();
+      String res = sumInfo.entrySet().stream()
+          .map(item -> String.format("%s: %s, %s", item.getKey(), item.getValue().getRight().getLeft(), item.getValue().getRight().getRight()))
           .collect(Collectors.joining("\n"));
-      System.out.println(String.format("---->%s\n%s\n<----", methodPath, res));
+      System.out.println(String.format("---->%s\n%s\n<----", invokerPath, res));
     }
     return object;
   }

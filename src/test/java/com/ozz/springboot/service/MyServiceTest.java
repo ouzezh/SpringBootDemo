60d4c29c1cbf609b68eb0d43a8285e4f82267f27
ouzezh <ouzezh@163.com>
2021-09-01 15:32:25 +0800

    remove jasypt

--- src/test/java/com/ozz/springboot/service/MyServiceTest.java
+++ src/test/java/com/ozz/springboot/service/MyServiceTest.java
@@ -1,6 +1,5 @@
 package com.ozz.springboot.service;
 
-import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,19 +8,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MyServiceTest {
   @Autowired
   MyService service;
-  @Autowired
-  StringEncryptor stringEncryptor;
+//  @Autowired
+//  StringEncryptor stringEncryptor;
 
   @Test
   void testService() {
     System.out.println(service.myService("p"));
   }
 
-  @Test
-  void testEncryptPwd() {
-    String result = stringEncryptor.encrypt("my config");
-    String str = stringEncryptor.decrypt(result);
-    System.out.println(String.format("%s -> %s", result, str));
-  }
+//  @Test
+//  void testEncryptPwd() {
+//    String result = stringEncryptor.encrypt("my config");
+//    String str = stringEncryptor.decrypt(result);
+//    System.out.println(String.format("%s -> %s", result, str));
+//  }
 
 }

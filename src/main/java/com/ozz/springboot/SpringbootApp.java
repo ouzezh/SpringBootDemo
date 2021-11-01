9e6a552748d19af2a1436337a0ef7aef887c0794
ouzezh <ouzezh@163.com>
2021-11-01 14:59:56 +0800

    config

--- src/main/java/com/ozz/springboot/SpringbootApp.java
+++ src/main/java/com/ozz/springboot/SpringbootApp.java
@@ -1,16 +1,28 @@
 package com.ozz.springboot;
 
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class SpringbootApp {
 
-  public static void main(String[] args) {
-    String profile = args.length>0 ? args[0].replace("--spring.profiles.active=", "") : "dev";
-    new SpringApplicationBuilder().sources(SpringbootApp.class).profiles(profile).run(args);
-  }
+    public static void main(String[] args) {
+        new SpringApplicationBuilder().sources(SpringbootApp.class).profiles(getProfile(args)).run(args);
+    }
+
+    private static String getProfile(String[] args) {
+        String prop = "--spring.profiles.active=";
+        String profile = args.length > 0 ? args[0].replace(prop, "") : null;
+        if(Strings.isNullOrEmpty(profile)) {
+            log.info("profile config {} not set, active default profile dev", prop);
+            return "dev";
+        }
+        return profile;
+    }
 
 }

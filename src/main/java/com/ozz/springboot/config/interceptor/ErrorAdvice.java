d35de34008174c26d1e19ca52993a7f455ee2aed
ouzezh <ouzezh@163.com>
2021-11-24 16:50:51 +0800

    config

--- src/main/java/com/ozz/springboot/config/interceptor/ErrorAdvice.java
+++ src/main/java/com/ozz/springboot/config/interceptor/ErrorAdvice.java
@@ -16,28 +16,28 @@ import java.util.Objects;
 @ControllerAdvice
 public class ErrorAdvice {
 
-  /**
-   * 处理异常
-   * <p>
-   * 强制指定返回状态码: @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-   */
-  @ExceptionHandler({WarnException.class})
-  @ResponseBody
-  public Map<String, Object> warnHandler(HttpServletResponse response, WarnException e) {
-    response.setStatus(e.getStatus().value());
-    return getMessage(e);
-  }
+    @ExceptionHandler({ErrorException.class})
+    @ResponseBody
+    public Map<String, Object> errorHandler(HttpServletResponse response, ErrorException e) {
+        log.error(null, e);
+        response.setStatus(e.getStatus().value());
+        return getMessage(e);
+    }
 
-  @ExceptionHandler({ErrorException.class})
-  @ResponseBody
-  public Map<String, Object> errorHandler(HttpServletResponse response, ErrorException e) {
-    log.error(null, e);
-    response.setStatus(e.getStatus().value());
-    return getMessage(e);
-  }
+    /**
+     * 处理异常
+     * <p>
+     * 强制指定返回状态码: @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+     */
+    @ExceptionHandler({WarnException.class})
+    @ResponseBody
+    public Map<String, Object> warnHandler(HttpServletResponse response, WarnException e) {
+        response.setStatus(e.getStatus().value());
+        return getMessage(e);
+    }
 
-  private Map<String, Object> getMessage(Throwable e) {
-    return Collections
-            .singletonMap("message", Objects.toString(e.getMessage(), e.getClass().getName()));
-  }
+    private Map<String, Object> getMessage(Throwable e) {
+        return Collections
+                .singletonMap("message", Objects.toString(e.getMessage(), e.getClass().getName()));
+    }
 }

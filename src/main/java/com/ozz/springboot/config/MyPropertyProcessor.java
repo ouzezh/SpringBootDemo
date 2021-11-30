c520f57114faf4d5b435ee1942a1e785f76145d3
ouzezh <ouzezh@163.com>
2021-11-30 14:45:15 +0800

    config

---
+++ src/main/java/com/ozz/springboot/config/MyPropertyProcessor.java
@@ -0,0 +1,47 @@
+package com.ozz.springboot.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+
+/**
+ * 配置文件覆盖配置中心的配置
+ */
+@Configuration
+public class MyPropertyProcessor implements BeanFactoryPostProcessor, EnvironmentAware, PriorityOrdered {
+
+    private ConfigurableEnvironment environment;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        MutablePropertySources psList = environment.getPropertySources();
+        for (PropertySource<?> ps : psList) {
+            if(ps.getName().matches(".*/config/application\\.(yml|properties).*")) {
+                psList.addFirst(ps);
+            }
+        }
+        for (PropertySource<?> ps : psList) {
+            System.out.println(ps.getName());
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = (ConfigurableEnvironment) environment;
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+
+}

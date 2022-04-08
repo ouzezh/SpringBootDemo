98aef907da9cdc6ea7fceb4aa393860b05e9b063
ouzezh <ouzezh@163.com>
2022-04-08 16:58:13 +0800

    u

--- src/main/java/com/ozz/springboot/SpringbootApp.java
+++ src/main/java/com/ozz/springboot/SpringbootApp.java
@@ -1,18 +1,34 @@
 package com.ozz.springboot;
 
-import com.ozz.springboot.util.SpringUtils;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.InetAddress;
 
 @Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class SpringbootApp {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApp.class, args);
+        ConfigurableApplicationContext app = SpringApplication.run(SpringbootApp.class, args);
+
+        Environment env = app.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }
--- src/main/java/com/ozz/springboot/vo/MyVo.java
+++ src/main/java/com/ozz/springboot/vo/MyVo.java
@@ -1,4 +1,4 @@
-package com.ozz.springboot.model;
+package com.ozz.springboot.vo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @ApiModel(value = "测试Model", description = "测试Model描述")
 @Data
-public class MyModel {
+public class MyVo {
     @ApiModelProperty(value = "名称", notes = "my notes", example = "n1", allowableValues = "n1 名称1, n2 名称2")
     private String name;
     @ApiModelProperty(hidden = true)
